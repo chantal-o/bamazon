edd3cf2bc0be2d6bd8d59dd97606dceae10b7ec0
Sushi C <chantal.otum@gmail.com>
2019-11-03 21:27:27 -0500

    updated js, installed dependencies that were not added with npm install

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,4 @@
-//Connecting mysql/inquirer
-
+// Establish connections
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -15,3 +14,24 @@ var connection = mysql.createConnection({
     if (err) throw err;
     createTable();
   });
+
+  //reference activity-topSongsAndAlbumsCode.js
+
+  var createTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+
+        for (var i = 0; i < res.length; i++) {
+
+            var Results = [
+                
+                "Product ID: " + res[i].item_id,
+                "Name of Product: " + res[i].product_name,
+                "Name of Department: " + res[i].department_name,
+                "Price: " + res[i].price,
+                "Stock Quantity: " + res[i].stock_quantity,
+            ]
+            console.log(Results); 
+        }
+        prompt(res);
+    })
+}
