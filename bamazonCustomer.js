8e211852d8055327d1dbea8a8d56193c5acf4798
Sushi C <chantal.otum@gmail.com>
2019-11-05 05:45:44 -0500

    fixed some issues with table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,12 +12,13 @@ var connection = mysql.createConnection({
  
  connection.connect(function(err) {
     if (err) throw err;
-    createTable();
+    // Call function to display all products 
+    displayTable();
   });
 
   //reference activity-topSongsAndAlbumsCode.js
-  // Display all of the items available for sale
-  var createTable = function () {
+  // Display all of the items available for sale and prompt to buy
+  var displayTable = function () {
     connection.query("SELECT * FROM products", function (err, res) {
 
         for (var i = 0; i < res.length; i++) {
@@ -32,23 +33,93 @@ var connection = mysql.createConnection({
             ]
             console.log(Results); 
         }
-        prompt(res);
+        buyprompt(res);
     })
 }
-//inquirer prompts for passing in questions
-function prompt() {
-    inquirer
-    .prompt({
-        name: "product",
-        type: "input",
-        message: "What product would you like to buy?"
-      }),
 
-      inquirer
-      .prompt([{
-        type: 'input',
-        name: 'quantity',
+
+function buyprompt() {
+    inquirer
+    .prompt(
+        {   name: "choice",
+            type: "list",
+            message: "Would you like to?",
+            choices: ["Make a Purchase", "Exit"],
+            
+        })
+        .then(function(answer) {
+           
+            var command = answer.choice;
+            
+            switch(command) {
+                //choice will begin database query
+                case "Make a Purchase":
+                    itemprompt();
+                break;
+                
+                // choice will end the connection
+                case "Exit":
+                    connection.end();
+            }
+        })
+}
+
+
+//inquirer prompts for passing in questions
+function itemprompt() {
+    inquirer
+    .prompt([
+        {
+        name: "itemID",
+        type: "input",
+        message: "What is the Item ID of the product you would you like to buy?"
+    },
+    {
+        name: "quantity",
+        type: "input",
         message: "How many units would you like to buy?"
     }])
+    .then(function(answer) {
 
+        // Use user's answers to prompts to query database for item stock_quantity and price
+        connection.query("SELECT stock_quantity, price FROM products WHERE ?",
+        {
+            item_id: answer.itemID
+        },
+
+        function(err, res) {
+            if(err) throw err;
+            else if (answer.item_id > 10) {
+                console.log("Insufficient quantity");
+             
+            displayTable();
+            }
+
+            else {
+                // Calculate cost of user's purchase by multiplying item price by desired quantity
+                cost = res[0].price * parseInt(answer.quantity);
+                
+                // Make UPDATE query to database to subtract necessary amount from stock_quantity
+                connection.query("UPDATE products SET ? WHERE ?",
+                [{
+                    stock_quantity: res[0].stock_quantity - parseInt(answer.itemNum)
+                },
+                {
+                    item_id: answer.itemID
+                }],
+                function(err, res) {
+                    if(err) throw err;
+                    
+                    // Log below message with total cost after successfully updating the database
+                   
+                    console.log(`${res.affectedRows}  The total cost of your purchase was $${cost.toFixed(2)}.`);
+                    
+
+                    // Call function to display all items in database again
+                    displayTable();
+                })                
+            }
+            
+        })
+    })
 }
